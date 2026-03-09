8cd8532bfe9e8a76b56bccbd199b8f5b20a34b14
felt-mx <jojoyi2603@gmail.com>
2026-03-09 14:23:29 +0800

    Increased websocket limit to 5mb

--- api/socket/server.py
+++ api/socket/server.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 
 # Create socket.io server with CORS enabled
 sio = socketio.AsyncServer(
-    async_mode="asgi", cors_allowed_origins="*", logger=True, engineio_logger=True
+    async_mode="asgi",
+    cors_allowed_origins="*",
+    logger=True,
+    engineio_logger=True,
+    max_http_buffer_size=5 * 1024 * 1024,  # 5 MB
 )
 
 
